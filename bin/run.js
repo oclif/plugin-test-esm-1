93481d6cffd27b5160d5184636138c4247e1606d
Mike Donnalley <mdonnalley@salesforce.com>
2023-08-09 16:57:01 -0600

    fix: bin/run shebang

--- bin/run.js
+++ bin/run.js
@@ -1,5 +1,6 @@
-#!/usr/bin/env node --loader ts-node/esm --no-warnings=ExperimentalWarning
+#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
 
+// eslint-disable-next-line node/shebang
 (async () => {
   const oclif = await import('@oclif/core')
   await oclif.execute({type: 'esm', dir: import.meta.url})
