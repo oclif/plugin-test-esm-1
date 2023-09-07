dffa5ab3b094583c54dd4329e75563ef249d465c
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-07 09:13:34 -0600

    fix: update bin/dev.js

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 // eslint-disable-next-line node/shebang
-void (async () => {
+(async () => {
   const oclif = await import('@oclif/core')
   await oclif.execute({development: true, dir: import.meta.url})
 })()
