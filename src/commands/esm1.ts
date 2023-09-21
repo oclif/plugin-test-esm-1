c006e0044f29eb63af79042f8966d16f03788890
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-21 13:17:03 -0600

    feat: add flags and args

--- src/commands/esm1.ts
+++ src/commands/esm1.ts
@@ -1,7 +1,36 @@
-import {Command} from '@oclif/core'
+import {Args, Command, Flags, Interfaces} from '@oclif/core'
+
+type Result = {
+  args: Interfaces.InferredArgs<typeof ESM1.args>
+  flags: Interfaces.InferredFlags<typeof ESM1.flags>
+}
 
 export default class ESM1 extends Command {
-  async run(): Promise<void> {
+  static flags = {
+    optionalString: Flags.string(),
+    defaultString: Flags.string({
+      default: 'simple string default',
+    }),
+    defaultFnString: Flags.string({
+      default: async () => Promise.resolve('async fn default'),
+    }),
+  }
+
+  static args = {
+    optionalArg: Args.string(),
+    defaultArg: Args.string({
+      default: 'simple string default',
+    }),
+    defaultFnArg: Args.string({
+      default: async () => Promise.resolve('async fn default'),
+    }),
+  }
+
+  static enableJsonFlag = true
+
+  async run(): Promise<Result> {
+    const {args, flags} = await this.parse(ESM1)
     this.log(`hello I am an ESM plugin from ${this.config.root}!`)
+    return {args, flags}
   }
 }
