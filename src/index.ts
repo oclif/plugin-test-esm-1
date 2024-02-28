ad46faaf3b893b3f16ada5ef69b24160b0224deb
Mike Donnalley <mdonnalley@salesforce.com>
2024-02-28 14:18:43 -0700

    feat: export hooks and commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export {run} from '@oclif/core'
+import ESM1 from './commands/esm1.js'
+import hook from './hooks/init/init.js'
+
+export const hooks = {
+  init: hook,
+}
+
+export const commands = {
+  esm1: ESM1,
+}
