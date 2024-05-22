778cc371ac1c2d5ebe22211ad03814352139454b
Mike Donnalley <mdonnalley@salesforce.com>
2024-05-22 10:12:41 -0600

    fix: use oclif/test v4

--- test/commands/esm1.test.ts
+++ test/commands/esm1.test.ts
@@ -1,10 +1,9 @@
-import {expect, test} from '@oclif/test'
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
 
-describe('hello', () => {
-  test
-  .stdout()
-  .command(['esm1'])
-  .it('runs esm1 cmd', ctx => {
-    expect(ctx.stdout).to.contain('hello I am an ESM plugin')
+describe('esm1', () => {
+  it('runs esm1 cmd', async () => {
+    const {stdout} = await runCommand(['esm1'])
+    expect(stdout).to.contain('hello I am an ESM plugin')
   })
 })
